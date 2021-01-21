ba398747b5e8f196a6e6ffe62b385d2cceb84777
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-01-21 12:15:38 +0530

    changed program

--- CuriousMind/src/main/java/TinyHelper/Practice.java
+++ CuriousMind/src/main/java/TinyHelper/Practice.java
@@ -2,6 +2,6 @@ package TinyHelper;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Heloo");
+        System.out.println("Hello Ashutosh Pandey");
     }
 }
